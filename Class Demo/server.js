405bc89fef5683c3250904e07daa009893396cd9
nidamon <59933097+nidamon@users.noreply.github.com>
2023-02-18 19:59:43 -0900

    Update server.js
    Put everything into functions.

--- Class Demo/server.js
+++ Class Demo/server.js
@@ -2,48 +2,83 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 const querystring = require('querystring');
+//const { realpath } = require('fs/promises');
 
 
-http.createServer(function (req, res) {
-  if (req.method === 'GET' && req.url === '/') {
-    // Read the contents of the HTML file and send it back to the client
-    fs.readFile('loginpage.html', function(err, data) {
-      if (err) {
-        res.writeHead(500, {'Content-Type': 'text/plain'});
-        res.end('Error reading file');
-      } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end(data);
-      }
-    });
-  } else if (req.method === 'POST' && req.url === '/submit') {
-    let body = '';
-    req.on('data', chunk => {
-      body += chunk.toString();
-    });
-    req.on('end', () => {
-      const formData = querystring.parse(body);
-      const uname = formData.textUname || '';
-      const pass = formData.textpass || '';
-      console.log(uname)
-      console.log(pass)
-      const html = `<!DOCTYPE html>
-        <html>
-          <head>
-            <title>Node.js Server Response</title>
-          </head>
-          <body>
-            <p>You entered for username: ${uname}</p>
-            <p>You entered for password: ${pass}</p>
-          </body>
-        </html>`;
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(html);
-    });
-  } else {
+http.createServer(function (req, res) {  
+  if (isRequested(req, 'GET', '/')) 
+  {
+    sendPage(res, getLoginPage());
+  } 
+  else if (isRequested(req, 'POST', '/submit')) 
+  {
+    handleLoginSubmission(req, res);
+  } 
+  else
+  {
     res.writeHead(404, {'Content-Type': 'text/plain'});
     res.end('Not found');
   }
-	
-  //res.end();
 }).listen(8080); 
+
+
+
+
+function isRequested(req, getPost, url)
+{
+    return req.method === getPost && req.url === url;
+}
+
+function sendPage(res, filename)
+{
+    // Read the contents of the HTML file and send it back to the client
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+          res.writeHead(500, {'Content-Type': 'text/plain'});
+          res.end('Error reading file');
+        } else {
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.end(data);
+        }
+    });
+}
+
+function getLoginPage()
+{
+  return 'loginpage.html';
+}
+
+function handleLoginSubmission(req, res)
+{
+  // Get form data
+  let body = '';
+  req.on('data', chunk => {
+    body += chunk.toString();
+  });
+
+  // Parse form data
+  req.on('end', () => {
+    const formData = querystring.parse(body);
+    const uname = formData.textUname || '';
+    const pass = formData.textpass || '';
+    console.log(uname)
+    console.log(pass)
+
+    // Returned response
+    const html = `<!DOCTYPE html>
+      <html>
+        <head>
+          <title>Node.js Server Response</title>
+        </head>
+        <body>
+          <p>You entered for username: ${uname}</p>
+          <p>You entered for password: ${pass}</p>
+        </body>
+      </html>`;
+
+
+
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end(html);
+  });
+}
