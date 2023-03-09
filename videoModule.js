50ce87746bdcb8cb7612a9ca4868eb84a8982746
William Glass <willg.glass94@gmail.com>
2023-03-08 23:52:01 -0900

    videoModule updated to slightly working

--- videoModule.js
+++ videoModule.js
@@ -1,17 +1,38 @@
 var dataBaseModule = require('./databaseModule.js')
+// Search for Genre: Lofi 
 
 
-// function to create HTML list of videos
+
 exports.createVideoList = async function() {
-    let html = '';
+    let html = '<h3>Lofi Genre</h3>';
+    let videos = await new Promise((resolve, reject) => {
+      dataBaseModule.getVideos("", "Lofi", function(videos) {
+        resolve(videos);
+      });
+    });
+    console.log(videos);
     for (let i = 0; i < videos.length; i++) {
-        html += `<div>
-            <h3>${videos[i].title}</h3>
-            <p>Uploaded by: ${videos[i].user}</p>
-            <p>Comment: ${videos[i].comment}</p>
-            ${videos[i].em}
-            <p><a href="/video/${videos[i]._id}">${videos[i].clickCount} clicks</a></p>
-            </div>`;
+      html += `<div>
+          <p>${videos[i].videoName}, Uploaded by: ${videos[i].videoUploader}</p>
+          ${videos[i].videoEmbedLink}
+          <p>Comment: ${videos[i].videoComment}</p>
+          <p><a href="/video/${videos[i]._id}">${videos[i].videoViewCount} clicks</a></p>
+          </div>`;
     }
-    return(html);
-};
+
+    html +=
+    `<div class="videoUploadBtn">  
+      <br>      
+      <button type='button' id='btnVideoUploadPage' onclick="window.location.href='videoupload.html'">Upload a video</button>
+    </div>`;
+    return html;
+  };
+  
+  
+  async function test() {
+    var result = await exports.createVideoList();
+    console.log(result);
+    console.log("hello");
+  }
+  test();
+  
