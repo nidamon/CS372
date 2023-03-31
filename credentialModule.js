f5632fe66862a04c40aa32ff6d1f7e8f5399a1de
nidamon <59933097+nidamon@users.noreply.github.com>
2023-03-31 02:03:45 -0800

    Update credentialModule.js
    Changed "yes" and "no" strings to be more descriptive of what they mean. Rewrote validateUser(...) to use guarded if statements.

--- credentialModule.js
+++ credentialModule.js
@@ -8,48 +8,68 @@ var dataBaseModule = require('./databaseModule.js')
 // Validates User credentials
 // Takes in a two stings username, password
 // and a dictionary of the logins (dict)
-exports.validateUser = function(uname, pass, callback_argBool_argStringMessage)
+exports.validateUser = async function(username, password, callback_argBool_argStringMessage)
 {
-  exports.isValidUsername(uname, function(isValid){
-    // Username valid
-    if(isValid) {
-      exports.isAccountLocked(uname, function(isLocked){
-        if(isLocked == false) {
-          exports.isPasswordCorrect(uname, pass, function(isCorrect){
-            // Password correct
-            if(isCorrect){
-              callback_argBool_argStringMessage(true, "Success");
-            } else {          
-              failedlogin(uname, function(message) {
-                callback_argBool_argStringMessage(false, 'The username or password is incorrect. ' + message);
-              });
-            }
-          }); // [End] - isPasswordCorrect callback          
-        } else { // Account is currently locked        
-          callback_argBool_argStringMessage(false, "Your account is currently locked");
-        }
-      }); // [End] - isAccountLocked callback      
-    } else {
-      // Username not valid
-      callback_argBool_argStringMessage(false, "Invalid Username");
-    }
-  }); // [End] - isValidUsername callback  
+  if(await invalidUsername(username, callback_argBool_argStringMessage))
+    return;
+
+  if(await accountLocked(username, callback_argBool_argStringMessage))
+    return;
+
+  if(await passwordCorrect(username, password, callback_argBool_argStringMessage))
+    return;
+
+  // Login success
+  callback_argBool_argStringMessage(true, "Success.");
+}
+
+// Promise wrapper for isValidUsername
+function invalidUsername(username, callback_argBool_argStringMessage){
+  return new Promise(resolve => {
+    exports.isValidUsername(username, function(isValid) {
+      if (!isValid)
+        callback_argBool_argStringMessage(false, "Invalid Username.");
+      resolve(!isValid);
+    });
+  });
+}
+// Promise wrapper for isAccountLocked
+function accountLocked(username, callback_argBool_argStringMessage){
+  return new Promise(resolve => {
+    exports.isAccountLocked(username, function(isLocked) {
+      if (isLocked)
+        callback_argBool_argStringMessage(false, "Your account is currently locked.");
+      resolve(isLocked);
+    });
+  });
+}
+// Promise wrapper for isPasswordCorrect
+function passwordCorrect(username, password, callback_argBool_argStringMessage){
+  return new Promise(resolve => {
+    exports.isPasswordCorrect(username, password, function(isCorrect) {
+      if (!isCorrect)
+        failedlogin(username, function(message) {
+          callback_argBool_argStringMessage(false, "The username or password is incorrect. " + message);
+        });
+      resolve(!isCorrect);
+    });
+  });
 }
 
 // Checks if username is present in the database
-exports.isValidUsername = async function(uname, callback_argBool)
+exports.isValidUsername = async function(uName, callback_argBool)
 {  
-  dataBaseModule.doesUserExist(uname, callback_argBool);
+  dataBaseModule.doesUserExist(uName, callback_argBool);
 }  
 
 // Checks if password is correct for the given username
-exports.isPasswordCorrect = function(uname, pass, callback_argBool)
+exports.isPasswordCorrect = async function(uName, pass, callback_argBool)
 {
   // Does given password match stored password
-  dataBaseModule.getUserFieldData(uname, "password", function(AccountPassword){
+  dataBaseModule.getUserFieldData(uName, "password", function(AccountPassword){
     isCorrect = AccountPassword == pass;    
     if(isCorrect)
-      exports.loginUser(uname);
+      exports.loginUser(uName);
 
     callback_argBool(isCorrect);
   })
@@ -61,7 +81,7 @@ exports.changePassword = async function(username, newPassword)
   // Change password
   dataBaseModule.editUserFieldData(username, "password", newPassword, function(){
     // Unlock account
-    dataBaseModule.editUserFieldData(username, "locked", "no", function(){
+    dataBaseModule.editUserFieldData(username, "locked", "notLocked", function(){
       // Reset failed logins
       dataBaseModule.editUserFieldData(username, "failedLogins", {
         "oldest": 0,
@@ -76,7 +96,7 @@ exports.changePassword = async function(username, newPassword)
 // Login handling
 exports.loginUser = function(username)
 {
-  dataBaseModule.editUserFieldData(username, "loggedIn", "yes");
+  dataBaseModule.editUserFieldData(username, "loggedIn", "loggedIn");
   console.log("User \"" + username + "\" has logged in.");
 }
 // Checks if the account tied to the username is currently logged in
@@ -84,13 +104,13 @@ exports.isLoggedin = function(username, callback_argBool)
 {
   console.log("Is user: " + username + " logged in?");
   dataBaseModule.getUserFieldData(username, "loggedIn", function(loggedInValue){
-    callback_argBool(loggedInValue == "yes"); // Passes true if user is logged in
+    callback_argBool(loggedInValue == "loggedIn"); // Passes true if user is logged in
   });
 }
 // Logout handling
 exports.logoutUser = function(username)
 {
-  dataBaseModule.editUserFieldData(username, "loggedIn", "no");
+  dataBaseModule.editUserFieldData(username, "loggedIn", "notLoggedIn");
   console.log("User \"" + username + "\" has logged out.");
 }
 
@@ -144,11 +164,11 @@ function threeFailedInAnHour(failedlogins)
   return failedlogins.newest - failedlogins.oldest < hour;
 }
 
-exports.isAccountLocked = function(username, callback_argBool)
+exports.isAccountLocked = async function(username, callback_argBool)
 {
-  dataBaseModule.getUserFieldData(username, "locked", function(data)
+  dataBaseModule.getUserFieldData(username, "locked", function(isLocked)
   {    
-    if(data == "no") // "no" == Not locked
+    if(isLocked == "notLocked")
       callback_argBool(false); // Say it's not locked
     else
       dataBaseModule.getUserFieldData(username, "failedLogins", function(failedLogins)
@@ -160,7 +180,7 @@ exports.isAccountLocked = function(username, callback_argBool)
         if(Date.now() - failedLogins.newest > day)
         {
           // 24 hours has passed -> unlock the account
-          dataBaseModule.editUserFieldData(username, "locked", "no", function(){
+          dataBaseModule.editUserFieldData(username, "locked", "notLocked", function(){
             callback_argBool(false); // Account is unlocked    
           });
         }
@@ -174,5 +194,5 @@ exports.isAccountLocked = function(username, callback_argBool)
 
 exports.lockAccount = function(username)
 {
-  dataBaseModule.editUserFieldData(username, "locked", "yes");
+  dataBaseModule.editUserFieldData(username, "locked", "locked");
 }
